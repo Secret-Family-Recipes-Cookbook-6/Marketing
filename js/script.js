1072197056ae386640990949a3f749ea72845bee
yirano <yirano@protonmail.ch>
2020-02-29 19:45:58 -0500

    Animate menu icon

--- js/script.js
+++ js/script.js
@@ -2,23 +2,30 @@ const socialLinks = document.querySelectorAll('.socialLinks');
 const aboutUsCard = document.querySelectorAll('.imgWrapper');
 const nav = document.querySelector('nav');
 const menuIcon = document.querySelector('.menu')
+const menuBarOne = document.querySelector('#spanOne');
+const menuBarTwo = document.querySelector('#spanTwo');
 
 if (window.innerWidth < 800) {
-  // nav.classList.add('hide');
+  nav.classList.add('hide');
 }
 window.addEventListener('resize', function () {
+
   if (window.innerWidth < 800) {
-    // nav.classList.add('hide');
+    nav.classList.add('hide');
   } else {
-    // nav.classList.remove('hide');
+    nav.classList.remove('hide');
   }
 })
 
 menuIcon.addEventListener('change', function () {
   if (menuIcon.checked) {
     nav.style = "top: 0%; transition: all 300ms linear;"
+    menuBarOne.style = 'transform: rotate(45deg); top: 13px; transition: transform 100ms linear';
+    menuBarTwo.style = 'transform: rotate(135deg); top: 10px;  transition: transform 100ms linear';
   } else {
     nav.style = "top: -100%; transition: all 300ms linear;"
+    menuBarOne.style = 'transform: rotate(0deg); top: 7px; transition: transform 100ms linear';
+    menuBarTwo.style = 'transform: rotate(0deg); top: 20px; transition: transform 100ms linear';
   }
 })
 console.log('hello');
