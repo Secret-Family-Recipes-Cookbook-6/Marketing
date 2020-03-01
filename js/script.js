b1f78df79754ab6037c435f7145c507488371047
yirano <yirano@protonmail.ch>
2020-02-29 19:51:39 -0500

    Refactor script code for menu icons - instead of styling inside the script sheet toggle classes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const socialLinks = document.querySelectorAll('.socialLinks');
 const aboutUsCard = document.querySelectorAll('.imgWrapper');
 const nav = document.querySelector('nav');
-const menuIcon = document.querySelector('.menu')
+const menuIcon = document.querySelector('.menu');
+const menuBars = document.querySelectorAll('span');
 const menuBarOne = document.querySelector('#spanOne');
 const menuBarTwo = document.querySelector('#spanTwo');
 
@@ -20,12 +21,14 @@ window.addEventListener('resize', function () {
 menuIcon.addEventListener('change', function () {
   if (menuIcon.checked) {
     nav.style = "top: 0%; transition: all 300ms linear;"
-    menuBarOne.style = 'transform: rotate(45deg); top: 13px; transition: transform 100ms linear';
-    menuBarTwo.style = 'transform: rotate(135deg); top: 10px;  transition: transform 100ms linear';
+    menuBars.forEach(x => {
+      x.classList.add('checked');
+    })
   } else {
     nav.style = "top: -100%; transition: all 300ms linear;"
-    menuBarOne.style = 'transform: rotate(0deg); top: 7px; transition: transform 100ms linear';
-    menuBarTwo.style = 'transform: rotate(0deg); top: 20px; transition: transform 100ms linear';
+    menuBars.forEach(x => {
+      x.classList.remove('checked');
+    })
   }
 })
 console.log('hello');
